cd3ba4446e77ad0da040d88f3eac5a00f6248633
bmurdock <brian.j.murdock@gmail.com>
2020-05-14 18:22:01 -0600

    fetch number of pokemon and use that to generate random number

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import settings from './settings';
 import Card from './components/Card';
 import './App.css';
@@ -12,26 +13,47 @@ class App extends React.Component {
     super();
     this.state = {
       displayedPokemon: {},
+      pokemonCount: 0,
     }
+  }
+
+  getPokemonCount = async () =>
+  {
+    let {count} = await fetch(`${settings.apiBaseRoute}/pokemon/`).then((response) => response.json());
+
+    this.setState({
+      pokemonCount: count,
+    });
   }
 
   getRandomPokemon = async () =>
   {
       // need to get a random WHOLE number between 1 and xxxxx
-      const number = Math.floor((Math.random() * 100)) + 1;
+      const number = Math.floor((Math.random() * this.state.pokemonCount)) + 1;
 
       const pokemonUrl = `${settings.apiBaseRoute}/pokemon/${number}`;
       const data = await fetch(pokemonUrl).then((response) => response.json());
-      // const data = fetch(pokemonUrl).then((respnose) => {return response.json()});
-      console.log(data);
+
       this.setState({
         displayedPokemon: data,
       });
   }
 
-  componentDidMount()
+  async componentDidMount()
   {
-    this.getRandomPokemon();
+    this.getPokemonCount()
+    .then(() =>
+    {
+      return this.getRandomPokemon();
+    })
+    .then(() =>
+    {
+      console.log('Successfully got the count and our first Pokemon.');
+    })
+    .catch((err) =>
+    {
+      console.log('Error getting the count or first Pokemon: ', err);
+    });
   }
 
   render()
